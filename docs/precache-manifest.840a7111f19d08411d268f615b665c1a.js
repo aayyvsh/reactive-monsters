2a8d5fa19a2b5cfaa63f32d2fd9206604b3e167b
aayyvsh <ayyvsh@gmail.com>
2020-07-31 01:54:33 -0400

    initial

---
+++ docs/precache-manifest.840a7111f19d08411d268f615b665c1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3995e6255d91a826b3e918e3ac78e81d",
+    "url": "/reactive-monsters/index.html"
+  },
+  {
+    "revision": "e42dd1313edb48e1eaea",
+    "url": "/reactive-monsters/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5c0b51b78832a90602a9",
+    "url": "/reactive-monsters/static/css/main.960c59c6.chunk.css"
+  },
+  {
+    "revision": "e42dd1313edb48e1eaea",
+    "url": "/reactive-monsters/static/js/2.881021f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactive-monsters/static/js/2.881021f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c0b51b78832a90602a9",
+    "url": "/reactive-monsters/static/js/main.f2ac3772.chunk.js"
+  },
+  {
+    "revision": "148260d132877af98b84",
+    "url": "/reactive-monsters/static/js/runtime-main.6d9b38ec.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  
+  const [pokelist, setPokelist] = useState([]);
+  const [todisplay, setTodisplay] = useState([]);
+  const [search, setSearch] = useState("");
+  const [pokedetails, setPokedetails] = useState({ name: "...", sprites: [] });
+
+  const getPokemon = e => {
+    axios.get("http://pokeapi.co/api/v2/pokemon?limit=1000")
+    .then(res => {
+      console.log(res.data.results);
+      setPokelist(res.data.results);
+      setTodisplay(res.data.results);
+    })
+    .catch(err => console.log(err));
+  }
+
+  const getDetails = pkmn => {
+    console.log(pkmn);
+    axios.get(pkmn.url)
+    .then(res => {
+      console.log(res.data.name);
+      setPokedetails(res.data);
+    })
+    .catch(err => console.log(err));
+  }
+
+  useEffect( () => {
+    getPokemon();
+
+  }, []);
+
+  useEffect( () => {
+      setTodisplay(pokelist.filter(p => p.name.includes(search)));
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="jumbotron">
+        <h3>Pokemon Are Neat</h3>
+      </div>
+      {/* <button className="btn btn-primary" onClick={getPokemon}>Get PKMN</button> */}
+
+
+      <div className="row">
+
+        <div className="col-sm-8">
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="search" onChange={e => setSearch(e.target.value)} />
+        </div>       
+        <ul className="list-group is-scrollable">
+          {todisplay.map( (pkmn, i) =>
+          <li key={i} className="list-group-item" onClick={e => getDetails(pkmn)}>{pkmn.name}</li>          
+          )}
+        </ul>
+      </div> 
+      <div className="col-sm-4">
+        <div className="card">
+          <div className="card-header bg-dark text-light">{pokedetails.name}</div>
+          <div className="card-body">
+            <img src={pokedetails.sprites.front_default} width="225px"/>
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
